77bc92db900abe2178a882fdd1920c0e9cc82f5f
caio.mmoura <caio.mmoura@dataprev.gov.br>
2026-03-13 16:04:05 -0300

    feat: Adição da galeria de arte com quadros e descrições

---
+++ quadrosposts.js
@@ -0,0 +1,12 @@
+const artGallery = {
+    "quadro1": {
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvlaHTBhTenZXx-WBF7egCPtvUFihjCBh8Tg&s",
+        title: "O amor imortal",
+        description: "Cruzando oceanos do tempo para encontrar aquilo que é eterno"
+    },
+    "quadro2": {
+        url: "https://images.unsplash.com/photo-1582555172866-f73bb12a2ab3?q=80&w=500",
+        title: "O Vazio Azul",
+        description: "..."
+    }
+};
